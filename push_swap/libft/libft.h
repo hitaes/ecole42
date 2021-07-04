849bc1e8f244cad2fc20807ec0d6704bef4a7c2b
hitaeskim <pacman.kakao.com>
2021-07-04 16:41:57 +0900

    push_swap

---
+++ push_swap/libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taeskim <taeskim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/14 13:33:23 by taeskim           #+#    #+#             */
+/*   Updated: 2021/06/15 11:28:20 by taeskim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdio.h>
+
+int		ft_isdigit(char c);
+int		ft_atoi(const char *src);
+int		ft_strlen(char *s);
+
+
+#endif
---
+++ push_swap/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taeskim <taeskim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/14 15:00:44 by taeskim           #+#    #+#             */
+/*   Updated: 2021/06/15 11:32:42 by taeskim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <limits.h>
+# include "./libft/libft.h"
+
+void	validator_num(char *s);
+
+#endif
