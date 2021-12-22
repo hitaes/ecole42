aa99ada56b5b6a46760ec6fa0b0004d53ffe4d18
taeskim <57426861+taeskim-42@users.noreply.github.com>
2021-12-22 15:24:43 +0900

    [2021-12-22]  try 4

--- circle2/pipex/srcs/shared/utils/pipe_utils.c
+++ circle2/pipex/srcs/shared/utils/pipe_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taeskim <taeskim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pacman <pacman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 01:20:07 by pacman            #+#    #+#             */
-/*   Updated: 2021/12/22 12:30:23 by taeskim          ###   ########.fr       */
+/*   Updated: 2021/12/22 15:24:26 by pacman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,18 +57,18 @@ void	outfile_to_pipe(t_arg *t, int i)
 	fd = file_open(t->command[i], O_CREAT);
 	// if (i % 2)
 	// {
-	// 	close(t->pipe_odd[1]);
-	// 	if (dup2(t->pipe_odd[0], STDIN_FILENO) == -1)
+	// 	close(t->p1[1]);
+	// 	if (dup2(t->p1[0], STDIN_FILENO) == -1)
 	// 		ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
 	// }
 	// else
 	// {
-		close(t->pipe_even[1]);
-		if (dup2(t->pipe_even[0], STDIN_FILENO) == -1)
-			ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
+	if (dup2(t->p2[0], STDIN_FILENO) == -1)
+		ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
 	// }
 	if (dup2(fd, STDOUT_FILENO) == -1)
 		ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
+	printf("✅Dec 22, 2021 12:35:46 PM \n");
 }
 
 void	child_proc_pipe(t_arg *t, int i)
@@ -77,34 +77,34 @@ void	child_proc_pipe(t_arg *t, int i)
 	// {
 	if (i == 1)
 	{
-		if (dup2(t->pipe_odd[1], STDOUT_FILENO) == -1)
+		if (dup2(t->p1[1], STDOUT_FILENO) == -1)
 			ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
-		close(t->pipe_odd[1]);
+		close(t->p1[1]);
 	}
 	else if (i == 2)
 	{
-		if (dup2(t->pipe_odd[0], STDIN_FILENO) == -1)
+		if (dup2(t->p1[0], STDIN_FILENO) == -1)
 			ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
-		// close(t->pipe_odd[0]);
-		if (dup2(t->pipe_even[1], STDOUT_FILENO) == -1)
+		// close(t->p1[0]);
+		if (dup2(t->p2[1], STDOUT_FILENO) == -1)
 			ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
-		// close(t->pipe_even[1]);
+		// close(t->p2[1]);
 	}
 	// else if (i == 2)
 	// {
-	// 	if (dup2(t->pipe_even[0], STDIN_FILENO) == -1)
+	// 	if (dup2(t->p2[0], STDIN_FILENO) == -1)
 	// 		ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
-	// 	close(t->pipe_even[0]);
+	// 	close(t->p2[0]);
 	// }
 	// }
 	// else
 	// {
-	// 	if (dup2(t->pipe_odd[0], STDIN_FILENO) == -1)
+	// 	if (dup2(t->p1[0], STDIN_FILENO) == -1)
 	// 		ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
-	// 	close(t->pipe_odd[0]);
-	// 	if (dup2(t->pipe_even[1], STDOUT_FILENO) == -1)
+	// 	close(t->p1[0]);
+	// 	if (dup2(t->p2[1], STDOUT_FILENO) == -1)
 	// 		ft_putstr_fd("Error : [DUP2: due2 failed]\n", STDERR_FILENO);
-	// 	close(t->pipe_even[1]);
+	// 	close(t->p2[1]);
 	// }
 }
 
@@ -115,11 +115,11 @@ void	command_process(t_arg *t, int i)
 
 	// if (i % 2)
 	if (i == 1)
-		pipe_open(t->pipe_odd);
+		pipe_open(t->p1);
 	else if (i == 2)
-		pipe_open(t->pipe_even);
+		pipe_open(t->p2);
 	// else
-	// 	pipe_open(t->pipe_even);
+	// 	pipe_open(t->p2);
 	pid = fork();
 	status = 0;
 	if (pid == -1)
@@ -132,18 +132,18 @@ void	command_process(t_arg *t, int i)
 	else
 	{
 		// if (i == 0)
-		// 	close(t->pipe_even[1]);
+		// 	close(t->p2[1]);
 		// else
 		// {
 		// 	if (i % 2)
 		// 	{
-		// 		close(t->pipe_even[0]);
-		// 		close(t->pipe_odd[1]);
+		// 		close(t->p2[0]);
+		// 		close(t->p1[1]);
 		// 	}
 		// 	else
 		// 	{
-		// 		close(t->pipe_even[1]);
-		// 		close(t->pipe_odd[0]);
+		// 		close(t->p2[1]);
+		// 		close(t->p1[0]);
 		// 	}
 		// }
 		waitpid(pid, &status, 0);
@@ -155,14 +155,40 @@ void	pipe_process(t_arg *t)
 	int	i;
 
 	i = 0;
-	infile_to_pipe(t, i);
-	++i;
-	// while (i < t->count)
-	// {
-	command_process(t, i);
-	++i;
-	command_process(t, i);
-	++i;
-	// }
-	outfile_to_pipe(t, i);
+	while (t->command[i])
+	{
+		if (i % 2)
+			if (pipe(t->p1) == -1)
+				printf("error pipe p1\n");
+		else
+			if (pipe(t->p2) == -1)
+				printf("error pipe p2\n");
+		if (i == 0)
+		{
+			int fd;
+
+			fd = open(t->command[0], O_RDONLY);
+			if (fd == -1)
+				printf("error open\n");
+			if (dup2(fd, STDIN_FILENO) == -1)
+				printf("error dup2")
+		}
+		i++;
+	}
+	
+	// 1. infile전에 파일 디스크립터를 이용해 파이프 셋팅 해야함.
+	// 2. fork()
+	// 3. infile에서 열었던 파일을 표준 입력과 출력으로 연결하기
+	// 3-1. 열었던 파일의 fd를 stdi에 연결시킴
+	// 3-2. 
+	// infile_to_pipe(t, i);
+	// ++i;
+	// // while (i < t->count)
+	// // {
+	// command_process(t, i);
+	// ++i;
+	// command_process(t, i);
+	// ++i;
+	// // }
+	// outfile_to_pipe(t, i);
 }
