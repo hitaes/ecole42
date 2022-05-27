61338d13af6c04f9077527ea482ac1232566b47c
taeskim <57426861+taeskim-42@users.noreply.github.com>
2022-05-27 14:48:29 +0900

    ex00

--- circle4/cpp/08/ex00/main.cpp
+++ circle4/cpp/08/ex00/main.cpp
@@ -1,8 +1,55 @@
+#include "easyfind.hpp"
+#include <list>
 
-#include <iostream>
-
-int main(void)
+int main()
 {
-  
-    return 0;
+
+    std::list<int> dummy;
+    std::list<int>::const_iterator	it;
+    bool existing = false;
+
+    dummy.push_back(1);
+    dummy.push_back(2);
+    dummy.push_back(3);
+
+    try
+    {
+        it = easyfind(dummy, 3);
+        existing = true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    if (existing)
+		std::cout << *it << " found" << std::endl;
+
+
+    existing = false;
+	try
+    {
+        it = easyfind(dummy, 1);
+        existing = true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    if (existing)
+		std::cout << *it << " found" << std::endl;
+
+    existing = false;
+	try
+    {
+        it = easyfind(dummy, 4);
+        existing = true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    if (existing)
+		std::cout << *it << " found" << std::endl;
+
+	return (0);
 }
